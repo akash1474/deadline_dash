0b0d5b4eea01219430a6d5f9a147648b736ec154
akash1474 <panditakash38@gmail.com>
2024-12-28 16:54:52 +0530

    updated resource file

--- premake5.lua
+++ premake5.lua
@@ -5,13 +5,12 @@ workspace "DeadlineDash"
    architecture "x64"
    configurations { "Debug", "Release", "Dist" }
 
-
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
-includeDirs={}
-includeDirs["glfw"]="packages/glfw/include"
-includeDirs["SpdLog"]="packages/spdlog/include"
-includeDirs["ImGui"]="packages/imgui"
+includeDirs = {}
+includeDirs["glfw"] = "packages/glfw/include"
+includeDirs["SpdLog"] = "packages/spdlog/include"
+includeDirs["ImGui"] = "packages/imgui"
 
 -- /MP -- Multithreaded build 
 -- /MT -- Static Linking. Defines _MT 
@@ -29,15 +28,13 @@ project "deadlinedash"
    pchsource "src/pch.cpp"
 
    links {
-      "glfw","ImGui","opengl32","dwmapi","Shlwapi","winmm","userenv","Shell32"
+      "glfw", "ImGui", "opengl32", "dwmapi", "Shlwapi", "winmm", "userenv", "Shell32"
    }
 
-   includedirs{
+   includedirs {
       "src",
       "%{includeDirs.glfw}",
       "%{includeDirs.ImGui}",
-      "%{includeDirs.Mini}",
-      "%{includeDirs.LunaSVG}",
       "%{includeDirs.SpdLog}"
    }
 
@@ -45,26 +42,34 @@ project "deadlinedash"
       "src/**.cpp"
    }
 
+   -- Add your manifest and resource file
+   files { 'setup.rc', '**.ico' }
+   vpaths { ['./*'] = { '*.rc', '*.manifest', '**.ico' } }
 
    filter "system:windows"
       systemversion "latest"
+
+   -- Post-build command to embed the manifest file
+   -- postbuildcommands {
+   --    'mt.exe -manifest "%{wks.location}DeadlineDash.manifest" -outputresource:"%{cfg.targetdir}/DeadlineDash.txt;1"'
+   -- }
 
    filter "configurations:Debug"
       runtime "Debug"
       symbols "On"
       staticruntime "On"
       optimize "Off"
-      buildoptions { "/MP","/DEBUG:FULL" }
-      defines {"GL_DEBUG"}
+      buildoptions { "/MP", "/DEBUG:FULL" }
+      defines { "GL_DEBUG" }
 
-   filter {"configurations:Release"}
+   filter "configurations:Release"
       runtime "Release"
       optimize "On"
       symbols "Off"
       characterset ("MBCS")
       staticruntime "On"
-      buildoptions { "/MP","/utf-8" }
-      defines {"GL_DEBUG","_CRT_SECURE_NO_WARNINGS"}
+      buildoptions { "/MP", "/utf-8" }
+      defines { "GL_DEBUG", "_CRT_SECURE_NO_WARNINGS" }
 
    filter "configurations:Dist"
       kind "WindowedApp"
@@ -73,7 +78,5 @@ project "deadlinedash"
       symbols "Off"
       characterset ("MBCS")
       staticruntime "On"
-      buildoptions { "/MP","/utf-8"}
-      linkoptions {"/ENTRY:mainCRTStartup"}
-      files { 'setup.rc', '**.ico' }
-      vpaths { ['./*'] = { '*.rc', '**.ico' }}
+      buildoptions { "/MP", "/utf-8" }
+      linkoptions { "/ENTRY:mainCRTStartup" }
